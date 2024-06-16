39ef1a407f6a6cfd256b98ff7eda80352c6a4692
Villasante Hilares Heli Antonio <hilares33v@gmail.com>
2024-06-16 02:01:18 -0300

    templet de login

--- src/app/pages/auth/pages/login/login.component.ts
+++ src/app/pages/auth/pages/login/login.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { BannerComponent } from './component/banner/banner.component';
+import { LoginFormComponent } from './component/login-form/login-form.component';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [BannerComponent, LoginFormComponent],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
